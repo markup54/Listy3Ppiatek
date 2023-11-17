f8b6cbe0adf65122b269568dac521a0f5d0b6c81
markup54 <marzenakupka@tlen.pl>
2023-11-17 11:42:38 +0100

    przycisk wstecz

--- app/src/main/java/pl/zabrze/zs10/listy3ppiatek/GaleriaActivity.java
+++ app/src/main/java/pl/zabrze/zs10/listy3ppiatek/GaleriaActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 public class GaleriaActivity extends AppCompatActivity {
     private ImageView imageView;
     private Spinner spinner;
-    private Button buttonWstecz, buttonDalej;
+    private Button buttonWstecz, buttonDalej,buttonWyswietl;
     private int aktualny = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +24,33 @@ public class GaleriaActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         buttonDalej = findViewById(R.id.buttonDalej);
         buttonWstecz = findViewById(R.id.buttonWstecz);
+        buttonWyswietl = findViewById(R.id.button4);
         int obrazki [] =new int[]{
                 R.drawable.szczeliniec,
                 R.drawable.bledne,
                 R.drawable.grzyb
         };
+        buttonWyswietl.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+                        int idRadioklikniety = radioGroup.getCheckedRadioButtonId();
+                        if(idRadioklikniety == R.id.radioButton){
+                            aktualny = 0;
+
+                        }
+                        else if(idRadioklikniety == R.id.radioButton2){
+                            aktualny = 1;
+                        }
+                        else {
+                            aktualny =2;
+                        }
+                        imageView.setImageResource(obrazki[aktualny]);
+
+                    }
+                }
+        );
         buttonWstecz.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
